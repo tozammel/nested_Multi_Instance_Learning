bf417fc5e71ef17981b36bdf8f3d2db38c291782
Tozammel Hossain <tozammel@isi.edu>
2017-08-01 20:53:42 -0700

    ed

--- datacuration/processjson.py
+++ datacuration/processjson.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import json
+
 __author__ = "Tozammel Hossain"
 __email__ = "tozammel@isi.edu"
 
@@ -10,3 +11,10 @@ def load_json(fpath, flat=False):
     with open(fpath, 'rb') as fh:
         decoded = json.loads(fh.read().decode('utf-8-sig'))
         return decoded if flat else decoded['values']
+
+
+def traverse_json_keys(ajson_obj, level=0):
+    for k, v in ajson_obj.items():
+        print('-' * level, k, sep="")
+        if isinstance(v, dict):
+            traverse_json_keys(v, level+1)
--- healthmap_data.py
+++ healthmap_data.py
@@ -4,7 +4,9 @@
 import os
 import json
 import pandas as pd
-
+from datacuration import io
+from datacuration import dataprocessing as dp
+from datacuration import processjson as pj
 
 __version__ = 1.0
 __author__ = "Tozammel Hossain"
@@ -29,11 +31,27 @@ Health map data format:
 ---place_list (list)
 """
 
-def load_healthmap_data():
-    gss_filepaths = io.load_filepaths_w_ext(gsrdir, 'json')
+
+def load_healthmap_data(gssdir):
+    gss_filepaths = io.load_filepaths_w_ext(gssdir, 'json')
+
+    for filepath in gss_filepaths[0:10]:
+        print("Loading:", filepath)
+        gss_data = dp.load_json(filepath, flat=True)
+        # pj.traverse_json_keys(gss_data)
+        # print(len(gss_data['data']))
+        for article in gss_data['data'][0:1]:
+            pj.traverse_json_keys(article)
+            # gsr_data.extend(gss_data)
+
+
+def analyze_healthmap_data():
+    datapath = "/Users/tozammel/safe/data/healthMap_GSS/data"
+    load_healthmap_data(datapath)
+
 
 def main(argv):
-    datapath = "/Users/tozammel/safe/data/healthMap_GSS/data"
+    analyze_healthmap_data()
 
 
 if __name__ == "__main__":
