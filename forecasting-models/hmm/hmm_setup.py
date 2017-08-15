b081864c03108abb30288e5e31ce0a0079c874a8
Tozammel Hossain <tozammel@isi.edu>
2017-08-15 10:42:19 -0700

    ed

--- forecasting-models/hmm/hmm_setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-from numpy.distutils.misc_util import get_info
-
-ext_modules = [Extension('_hmmc', ['_hmmc.pyx'],
-                         extra_compile_args=["-O3"], **get_info("npymath"))]
-
-setup(name='_hmmc', cmdclass={'build_ext': build_ext}, ext_modules=ext_modules)
--- forecasting-models/kde/adaptivekde/__init__.py
+++
@@ -1,9 +0,0 @@
-from .sshist import sshist
-from .sskernel import sskernel
-from .ssvkernel import ssvkernel
-
-__version__ = '1.0.0'
-
-__all__ = ('sshist',
-           'sskernel',
-           'ssvkernel')
